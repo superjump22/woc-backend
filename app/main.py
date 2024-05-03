99c7a06cabd2f8c69595dc618554d0d9745662fb
Super Jump <guo.c14.tech@gmail.com>
2024-05-03 19:30:54 +0800

    test

--- app/main.py
+++ app/main.py
@@ -20,7 +20,8 @@ def update_docker_image(container_name: str):
     print(datetime.now())
     print(f"updating docker image for container {container_name}", flush=True)
     client = docker.from_env()
-    client.containers.run(image="containrrr/watchtower", command=["--run-once", container_name], auto_remove=True, detach=True, remove=True, volumes=['/var/run/docker.sock:/var/run/docker.sock'])
+    client.containers.run(image="containrrr/watchtower", command=["--run-once", container_name],
+                          auto_remove=True, detach=True, remove=True, volumes=['/var/run/docker.sock:/var/run/docker.sock'])
     client.images.prune()
 
 
@@ -53,7 +54,7 @@ async def get_schedule_jobs():
     jobs = []
     for job in scheduler.get_jobs():
         print(u'    id: %s, %s' % (job.id, job))
-        jobs.append(u'id: %s, %s' % (job.id, job))
+        jobs.append({"id": job.id, "content": u'%s' % job})
     return jobs
 
 
@@ -61,7 +62,8 @@ async def get_schedule_jobs():
 async def add_schedule_job(job: ScheduleJob):
     if scheduler.get_job(job.id) != None:
         scheduler.remove_job(job.id)
-    scheduler.add_job(id=job.id, func=update_docker_image, args=[job.container_name], trigger=job.trigger, **job.trigger_args)
+    scheduler.add_job(id=job.id, func=update_docker_image, args=[job.container_name],
+                      trigger=job.trigger, **job.trigger_args)
     return await get_schedule_jobs()
 
 
@@ -74,7 +76,7 @@ async def get_schedule_job(job_id: str):
         print('None', flush=True)
         return 'None'
     print(u'%s' % job, flush=True)
-    return u'%s' % job
+    return {"id": job.id, "content": u'%s' % job}
 
 
 @app.delete("/scheduler/jobs/{job_id}")
