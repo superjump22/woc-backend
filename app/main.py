e6bbba0cd02885ad653b4deb9f59a325172984f0
Super Jump <guo.c14.tech@gmail.com>
2024-05-03 16:55:19 +0800

    test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from contextlib import asynccontextmanager
+from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Any
@@ -14,6 +15,7 @@ class ScheduleJob(BaseModel):
 
 
 def update_docker_image(container_name: str):
+    print(datetime.now())
     print(f"Updating docker image for container {container_name}")
 
 
@@ -43,13 +45,15 @@ async def home():
     return {"message": "Hello Woc!"}
 
 
-@app.get("/schedule/jobs/")
+@app.get("/scheduler/jobs/")
 async def get_schedule_jobs():
     scheduler.print_jobs()
     return {"jobs": len(scheduler.get_jobs())}
 
 
-@app.post("/schedule/jobs/")
-async def create_schedule_job(job: ScheduleJob):
+@app.post("/scheduler/jobs/")
+async def add_schedule_job(job: ScheduleJob):
+    if scheduler.get_job(job.id) != None:
+        scheduler.remove_job(job.id)
     scheduler.add_job(id=job.id, func=update_docker_image, args=[job.container_name], trigger=job.trigger, **job.trigger_args)
-    return {"jobs": len(scheduler.get_jobs())}
+    return get_schedule_jobs()
